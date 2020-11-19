55f92376d1316f25098e9fad6602862ecf31c98b
edneyRoldao <edneyroldao@gmail.com>
2020-11-19 07:20:59 -0300

    ajustes

--- 01-infinite-scroll/src/app/infinite-scroll/back-end.server.js
+++ 01-infinite-scroll/src/app/infinite-scroll/back-end.server.js
@@ -19,10 +19,70 @@ const people = [
   {name: 'Ricardo17', last_name: 'Silva'},
   {name: 'Ricardo18', last_name: 'Silva'},
   {name: 'Ricardo19', last_name: 'Silva'},
-
+  {name: 'Ricardo', last_name: 'Silva'},
+  {name: 'Ricardo1', last_name: 'Silva'},
+  {name: 'Ricardo2', last_name: 'Silva'},
+  {name: 'Ricardo3', last_name: 'Silva'},
+  {name: 'Ricardo4', last_name: 'Silva'},
+  {name: 'Ricardo5', last_name: 'Silva'},
+  {name: 'Ricardo6', last_name: 'Silva'},
+  {name: 'Ricardo7', last_name: 'Silva'},
+  {name: 'Ricardo8', last_name: 'Silva'},
+  {name: 'Ricardo9', last_name: 'Silva'},
+  {name: 'Ricardo10', last_name: 'Silva'},
+  {name: 'Ricardo11', last_name: 'Silva'},
+  {name: 'Ricardo12', last_name: 'Silva'},
+  {name: 'Ricardo13', last_name: 'Silva'},
+  {name: 'Ricardo14', last_name: 'Silva'},
+  {name: 'Ricardo15', last_name: 'Silva'},
+  {name: 'Ricardo16', last_name: 'Silva'},
+  {name: 'Ricardo17', last_name: 'Silva'},
+  {name: 'Ricardo18', last_name: 'Silva'},
+  {name: 'Ricardo19', last_name: 'Silva'},
+  {name: 'Ricardo', last_name: 'Silva'},
+  {name: 'Ricardo1', last_name: 'Silva'},
+  {name: 'Ricardo2', last_name: 'Silva'},
+  {name: 'Ricardo3', last_name: 'Silva'},
+  {name: 'Ricardo4', last_name: 'Silva'},
+  {name: 'Ricardo5', last_name: 'Silva'},
+  {name: 'Ricardo6', last_name: 'Silva'},
+  {name: 'Ricardo7', last_name: 'Silva'},
+  {name: 'Ricardo8', last_name: 'Silva'},
+  {name: 'Ricardo9', last_name: 'Silva'},
+  {name: 'Ricardo10', last_name: 'Silva'},
+  {name: 'Ricardo11', last_name: 'Silva'},
+  {name: 'Ricardo12', last_name: 'Silva'},
+  {name: 'Ricardo13', last_name: 'Silva'},
+  {name: 'Ricardo14', last_name: 'Silva'},
+  {name: 'Ricardo15', last_name: 'Silva'},
+  {name: 'Ricardo16', last_name: 'Silva'},
+  {name: 'Ricardo17', last_name: 'Silva'},
+  {name: 'Ricardo18', last_name: 'Silva'},
+  {name: 'Ricardo19', last_name: 'Silva'},
+  {name: 'Ricardo', last_name: 'Silva'},
+  {name: 'Ricardo1', last_name: 'Silva'},
+  {name: 'Ricardo2', last_name: 'Silva'},
+  {name: 'Ricardo3', last_name: 'Silva'},
+  {name: 'Ricardo4', last_name: 'Silva'},
+  {name: 'Ricardo5', last_name: 'Silva'},
+  {name: 'Ricardo6', last_name: 'Silva'},
+  {name: 'Ricardo7', last_name: 'Silva'},
+  {name: 'Ricardo8', last_name: 'Silva'},
+  {name: 'Ricardo9', last_name: 'Silva'},
+  {name: 'Ricardo10', last_name: 'Silva'},
+  {name: 'Ricardo11', last_name: 'Silva'},
+  {name: 'Ricardo12', last_name: 'Silva'},
+  {name: 'Ricardo13', last_name: 'Silva'},
+  {name: 'Ricardo14', last_name: 'Silva'},
+  {name: 'Ricardo15', last_name: 'Silva'},
+  {name: 'Ricardo16', last_name: 'Silva'},
+  {name: 'Ricardo17', last_name: 'Silva'},
+  {name: 'Ricardo18', last_name: 'Silva'},
+  {name: 'Ricardo19', last_name: 'Silva'}
 ];
 
-let number = 0;
+let init = 0;
+let end = 10;
 
 const express = require("express");
 
@@ -38,17 +98,13 @@ app.use(function(req, res, next) {
 })
 
 app.get("/pessoas", (req, res) => {
-  number++;
-  console.log(number)
-  if(number == 1){
 
-  }
-  let filter = people.slice(0,10)
+  const lista = people.slice(init, end);
+  init += 10;
+  end += 10;
 
-
-  res.status(200).json(filter);
+  res.status(200).json(lista);
 });
-
 
 app.listen(3030, () => {
   console.log('server working');
